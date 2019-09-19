8de8af70a4d8b6428a9778acd6a1ef5fdf9e44ec
SmilingCode <ericc_j@icloud.com>
2019-09-20 02:01:35 +1000

    root /

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
-const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
   entry: './src/App.js',
   output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/',
+        path: __dirname,
+        publicPath: '/',
         filename: 'bundle.js'
   },
   module: {
@@ -40,10 +35,5 @@ module.exports = {
         use:['style-loader','css-loader', 'sass-loader']
       },
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+  }
 };
