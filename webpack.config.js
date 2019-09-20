41ea40686cd25d9d8888984c3cdd3f34568cc340
SmilingCode <ericc_j@icloud.com>
2019-09-20 14:06:24 +1000

    compress files setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
+const isDevelopment = process.env.NODE_ENV === 'development';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
+  mode: 'development',
   entry: './src/App.js',
   output: {
-        path: __dirname,
-        publicPath: '/',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
         filename: 'bundle.js'
   },
   module: {
@@ -26,14 +31,54 @@ module.exports = {
       },
       // css handler
       {
-        test: /\.css$/,
-        use:['style-loader','css-loader']
+         test:/\.(s*)css$/,
+         use:['style-loader','css-loader', 'sass-loader']
       },
       // sass handler
       {
-        test: /\.scss$/,
-        use:['style-loader','css-loader', 'sass-loader']
+        test: /\.module\.s(a|c)ss$/,
+        loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+        ],
+      },
+
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+        ]
       },
     ]
-  }
+  },
+  // specify files
+  plugins: [
+      new MiniCssExtractPlugin({
+          filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+          chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+      }),
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      })
+  ],
 };
